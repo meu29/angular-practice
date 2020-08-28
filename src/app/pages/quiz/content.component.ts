eae487deb45b8482514746a43ea8197ee2c1b15c
meu29 <meu29@ezweb.ne.jp>
2020-08-28 21:29:22 +0900

    expressと接続

--- src/app/pages/quiz/content.component.ts
+++ src/app/pages/quiz/content.component.ts
@@ -10,25 +10,33 @@ import { DataService } from "/home/meu/ドキュメント/angular-practice/Inter
 })
 
 //OnInit インターフェースには ngOnInit() という名前のフックメソッドがあります。
-export class quizContentComponent { //implements OnInit { 
+export class quizContentComponent implements OnInit { 
 
   question: string;
   choices: string[];
   correctAnswer: string;
-  question_index: number = 0;
+  question_index: number;
   question_number: number;  
-  correct_answer_number: number = 0;
-  has_next: boolean = true;
-  has_lifeline_right: boolean = true;
+  correct_answer_number: number;
+  has_next: boolean;
+  has_lifeline_right: boolean;
   
   /* constructor以外でsubscribe()をやるとエラーが発生 */
   constructor(private dataService: DataService) { }
   
-  /*
+  /* コンポーネントの初期化時にreset()メソッドを自動で実行 */
   ngOnInit() {
-    this.createNextQuiz();
+    this.reset();
   }
-  */
+
+  /* ngOnInit()で自動実行できるのは1度だけなので、2回目以降の初期化(再チャレンジ)は手動(ボタンを押す)で実行 */
+  reset() {
+    this.correct_answer_number = 0;
+    this.question_index = 0;
+    this.question_number = undefined;
+    this.has_next = true;
+    this.has_lifeline_right = true;
+  }    
 
   createNextQuiz() {
     this.dataService.getData().subscribe(obj => {
@@ -38,7 +46,6 @@ export class quizContentComponent { //implements OnInit {
         document.getElementsByClassName("one_ans")[0].removeAttribute("disabled");
         document.getElementsByClassName("one_ans")[0].setAttribute("style", "background-color: red");
       }
-      alert(JSON.stringify(obj));
       this.question_number = Object.keys(obj).length;
       this.question = obj[this.question_index]["question"];
       this.choices = obj[this.question_index]["answer_candidates"];
@@ -47,14 +54,24 @@ export class quizContentComponent { //implements OnInit {
   }
 
   useLifeline() {
+
     if (this.has_lifeline_right == true) {
-      document.getElementsByClassName("one_ans")[0].setAttribute("disabled", "disabled");
-      document.getElementsByClassName("one_ans")[0].setAttribute("style", "background-color: white");
+      var correct_answer_index: number = this.choices.indexOf(this.correctAnswer);
+      var delete_answer_index: number;
+      if (correct_answer_index == -1) {
+        delete_answer_index = Math.floor(Math.random() * Math.floor(6));
+      } else {
+        //これだと正解を消してしまうことがある
+        delete_answer_index = Math.floor(Math.random() * Math.floor(7));
+      }
+      document.getElementsByClassName("one_ans")[delete_answer_index].setAttribute("disabled", "disabled");
+      document.getElementsByClassName("one_ans")[delete_answer_index].setAttribute("style", "background-color: white");
       document.getElementById("lifeline").setAttribute("style", "background-color: gray");
       this.has_lifeline_right = false;
     } else {
       alert("ライフラインは一度しか使えません!");
     }
+
   }
   
   register(answer) {
@@ -70,10 +87,5 @@ export class quizContentComponent { //implements OnInit {
     this.question_index += 1;
     this.createNextQuiz();
   }
-
-  reset() {
-    this.correct_answer_number = 0;
-    this.question_number = undefined;
-  }  
 
 }
